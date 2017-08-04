b46e509cb9d4187df534058c4d36198f84efa1a4
Chiranjiv-Roy <chiranjiv.roy@lexmark.com>
2017-08-04 20:14:29 +0530

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Navigationbar from './navbar'
+import Sidebar from './sidebar'
+
 
 export default class App extends Component {
+	
   render() {
     return (
-      <div>React simple starter</div>
+    	<div>
+    		<Sidebar />
+    		<Navigationbar />
+    	</div>
     );
   }
 }
